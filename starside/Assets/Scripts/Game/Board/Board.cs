e13a93f1958b376d2d4c02fca404a668da92b337
David Zhang <david8zhang@berkeley.edu>
2015-10-07 12:53:06 -0700

    added aimer files

--- starside/Assets/Scripts/Game/Board/Board.cs
+++ starside/Assets/Scripts/Game/Board/Board.cs
@@ -4,22 +4,22 @@ using System.Collections;
 public class Board : MonoBehaviour {
     
     //Board Attributes
+    public bool populated = false; 
     private const int boardSize = 10;
 
     //Actual board itself
-    private int boardGen;
+    private int[,] boardGen;
     private BoardTile[,] gridOutline;
 
     //Prefab objects
     public GameObject tilePrefab;
-
-    public bool populated = false; 
         
     //Use this for pre-initialization
     void Awake()
     {
         //Initalize the gridOutline
         gridOutline = new BoardTile[boardSize, boardSize];
+        boardGen = new int[boardSize, boardSize];
     }
 
     //Initalize the board
@@ -32,6 +32,26 @@ public class Board : MonoBehaviour {
                 gridOutline[j, i] = CreateNewTile(tilePrefab, i, j).GetComponent<BoardTile>() as BoardTile;
             }
         }
+    }
+
+    //Assign numerical values to all elements on board
+    public void PopulateBoard()
+    {
+        for(int x = 0; x < boardSize; x++)
+        {
+            for(int y = 0; y < boardSize; y++)
+            {
+                boardGen[y, x] = 0; 
+            }
+        }
+        populated = true; 
+
+        //Create enemies here
+    }
+
+    public bool isPopulated()
+    {
+        return populated; 
     }
 	
     // Use this for initialization
